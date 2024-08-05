c5ec240645a951d72f10ea07033c6fd1cbd32b4b
hw <hw710204@gmail.com>
2024-08-05 11:57:51 +0900

    modified file and GitHub update test try V5

--- src/main/java/com/itgroup/application/Layout01.java
+++ src/main/java/com/itgroup/application/Layout01.java
@@ -7,6 +7,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Layout01 extends Application {
+    // try push first V5
     // GitHub test text
     // 수정 및 변동 시 특정 파일만 변경하여 푸쉬해보기 V1
     // 원본 수정하면 다운로드 받을 때 어떻게 되는가? V3
