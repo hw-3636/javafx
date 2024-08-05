d29e0f0b90f16eba9e8915ef7f84a85997cb5211
hw <hw710204@gmail.com>
2024-08-05 11:04:11 +0900

    modified file and GitHub update test V2

--- src/main/java/com/itgroup/application/Layout02.java
+++ src/main/java/com/itgroup/application/Layout02.java
@@ -10,6 +10,8 @@ import javafx.stage.Stage;
 import java.util.Scanner;
 
 public class Layout02 extends Application {
+    // GitHub test text V2
+    // 수정 및 변동 시 특정 파일만 변경하여 푸쉬 V2
 
     @Override
     public void start(Stage stage) throws Exception {
