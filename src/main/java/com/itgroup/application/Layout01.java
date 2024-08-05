08d3187e3b03fc7d86c83664355831572be053cf
hw <hw710204@gmail.com>
2024-08-05 11:33:40 +0900

    modified file and GitHub update test V3

--- src/main/java/com/itgroup/application/Layout01.java
+++ src/main/java/com/itgroup/application/Layout01.java
@@ -8,7 +8,8 @@ import javafx.stage.Stage;
 
 public class Layout01 extends Application {
     // GitHub test text
-    // 수정 및 변동 시 특정 파일만 변경하여 푸쉬해보기
+    // 수정 및 변동 시 특정 파일만 변경하여 푸쉬해보기 V1
+    // 원본 수정하면 다운로드 받을 때 어떻게 되는가? V3
     @Override
     public void start(Stage stage) throws Exception {
         String fxmlFile = "/com/itgroup/fxml/" + "Layout01.fxml";
